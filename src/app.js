77fc6cb33bf675a4aff422c940f04997edf8a883
Ricardo Franco <ricardofranco2509@gmail.com>
2023-08-17 16:38:35 -0300

    incluindo o schema do banco

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import db from "./config/dbConnect.js"
+import livros from "./models/Livro.js"
 
 db.on("error", console.log.bind(console, 'erro de conexão'))
 db.once("open", () => {
@@ -10,17 +11,14 @@ const app = express()
 
 app.use(express.json())
 
-const livros = [
-  {id: 1, "titulo": "Senhor dos Anéis"},
-  {id: 2, "titulo": "O Hobbit"}
-]
-
 app.get('/', (req, res) => {
   res.status(200).send('Curso de Node')
 })
 
 app.get('/livros', (req, res) => {
-  res.status(200).json(livros)
+  livros.find((err, livros) => {
+    res.status(200).json(livros)
+  })
 })
 
 app.get('/livros/:id', (req, res) => {
